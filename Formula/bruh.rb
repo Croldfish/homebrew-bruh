486c96ec6f32d8b4a22bca6b5d79c25f051c4ddc
Croldfish <70867293+Croldfish@users.noreply.github.com>
2022-04-16 11:00:41 -0400

    Create bruh.rb

---
+++ Formula/bruh.rb
@@ -0,0 +1,11 @@
+class bruh < Formula
+  desc "it really hits hard"
+  homepage "github.com/Croldfish/bruh"
+  url "https://github.com/Croldfish/bruh/releases/latest/download/bruh.tar.gz"
+  sha256 "f9a393857ba0b7edb67d1b02cbaae63c970768d63a354fc1f15b129035b7a554"
+  version "0.1.69"
+
+  def install
+    bin.install "bruh"
+  end
+end
