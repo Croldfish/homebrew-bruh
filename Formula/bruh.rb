fecc78237d852e89f9764647be9475135f3fc43b
Croldfish <70867293+Croldfish@users.noreply.github.com>
2022-04-21 14:26:33 -0400

    Update bruh.rb

--- Formula/bruh.rb
+++ Formula/bruh.rb
@@ -2,8 +2,8 @@ class Bruh < Formula
   desc "it really hits hard"
   homepage "github.com/Croldfish/bruh"
   url "https://github.com/Croldfish/bruh/releases/latest/download/bruh.tar.gz"
-  sha256 "f9a393857ba0b7edb67d1b02cbaae63c970768d63a354fc1f15b129035b7a554"
-  version "0.1.69"
+  sha256 "ffc8cab23bcf5eacadba278d516a6be8b2b99e2b39bea750dacc0e228cccce8d"
+  version "0.1.70"
 
   def install
     bin.install "bruh"
